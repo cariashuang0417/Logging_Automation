1eae07beaf55fd5a07300e3b53bf222b704c1bfe
Shuang Song <103439749+cariashuang0417@users.noreply.github.com>
2023-02-25 01:04:52 -0500

    Change code format and add comments for better presenting

--- app.py
+++ app.py
@@ -21,50 +21,39 @@ def get_gis(event):
 
 
 def get_logs(gis):
+    """
+    Query logs from portal and servers
+    """
+
+    # set up object types
     admin: PortalAdminManager = gis.admin
     servers: dict[str, list[str, Any]] = {admin.url: []}
     days_back: int
     ref: dict[str, Any] = {admin.url: admin.logs}
     start: _dt.datetime = _dt.datetime.now()
     jobs = {}
+
+    # concurrent jobs to get logs
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as tp:
-        # gather the portal logs
-        #
+
+        # get logs settings from portal
         days_back = admin.logs.settings.get("maxLogFileAge", 90)
+
+        # gather the portal logs
         for d in range(days_back):
-            future = tp.submit(
-                admin.logs.query,
-                **{
-                    "start_time": start - _dt.timedelta(days=d),
-                    "end_time": start - _dt.timedelta(days=d + 1),
-                },
-            )
-            jobs[future] = {
-                "server": admin.url,
-                'start_time': start - _dt.timedelta(days=d),
-                'end_time': start - _dt.timedelta(days=d + 1),
-            }
-        # gather the server logs
-        #
+            future = tp.submit(admin.logs.query, **{"start_time": start - _dt.timedelta(days=d), "end_time": start - _dt.timedelta(days=d + 1)})
+            jobs[future] = {"server": admin.url, 'start_time': start - _dt.timedelta(days=d), 'end_time': start - _dt.timedelta(days=d + 1)}
+
+        # loop through all federated servers and gather the server logs
         for server in admin.servers.list():
             servers[server.url] = []
             ref[server.url] = server
             days_back = server.logs.settings.get("maxLogFileAge", 90)
             for d in range(days_back):
-                future = tp.submit(
-                    server.logs.query,
-                    **{
-                        "start_time": start - _dt.timedelta(days=d),
-                        "end_time": start - _dt.timedelta(days=d + 1),
-                    },
-                )
-                jobs[future] = {
-                    "server": server.url,
-                    'start_time': start - _dt.timedelta(days=d),
-                    'end_time': start - _dt.timedelta(days=d + 1),
-                }
-        #  Push the log entries into the servers.
-        #
+                future = tp.submit(server.logs.query, **{"start_time": start - _dt.timedelta(days=d), "end_time": start - _dt.timedelta(days=d + 1)})
+                jobs[future] = {"server": server.url, 'start_time': start - _dt.timedelta(days=d), 'end_time': start - _dt.timedelta(days=d + 1)}
+
+        #  loop through the concurrent jobs and push the log entries into the servers.
         for job in concurrent.futures.as_completed(jobs):
             try:
                 records = job.result()
@@ -84,23 +73,34 @@ def get_logs(gis):
         return servers
 
 
+def put_data_to_s3(data):
+    """
+    push logs to s3 bucket
+    """
+
+    # set up file name in s3
+    DEFAULT_FILENAME = f'logs-{int(_dt.datetime.utcnow().timestamp())}.json'
+
+    # parse logs to json format
+    body = get_json_bytes(data)
+
+    # connect to s3 client
+    s3_client = boto3.client('s3')
+
+    # put logs to s3 with a time stamp
+    s3_client.put_object(
+        Body=body,
+        Bucket=os.environ.get('AWS_S3_BUCKET', 'devsummit-logging-archive'),
+        Key=os.environ.get('AWS_S3_LOG_FILENAME', DEFAULT_FILENAME)
+    )
+
+
 def get_json_bytes(data):
     """
     Get JSON as binary for provided input data
     """
     json_pretty_utf8_str = json.dumps(data, indent=4).encode('utf-8')
     return json_pretty_utf8_str
-
-
-def put_data_to_s3(data):
-    DEFAULT_FILENAME = f'logs-{int(_dt.datetime.utcnow().timestamp())}.json'
-    body = get_json_bytes(data)
-    s3_client = boto3.client('s3')
-    s3_client.put_object(
-        Body=body, 
-        Bucket=os.environ.get('AWS_S3_BUCKET', 'devsummit-logging-archive'), 
-        Key=os.environ.get('AWS_S3_LOG_FILENAME', DEFAULT_FILENAME)
-    )
 
 
 def get_main_args():
